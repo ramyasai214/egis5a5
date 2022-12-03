34b112f25d90b445767a566f1da46d122493d225
chand <chand@MSI>
2022-12-03 20:14:47 +0530

    5a5

---
+++ src/test/java/egjs5a5/egis5a5/AppTest.java
@@ -0,0 +1,20 @@
+package egjs5a5.egis5a5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
